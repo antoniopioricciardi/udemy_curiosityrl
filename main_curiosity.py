5df092f806fe2144ff4f64cce4a9ee849d0120b2
Antonio Pio Ricciardi <antonioricciardi93@gmail.com>
2022-09-14 15:20:58 +0200

    bugfix

--- main_curiosity.py
+++ main_curiosity.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     # env_id = 'PongNoFrameskip-v4'
     env_id = 'MiniWorld-Hallway-v0'
     # env_id = 'MiniWorld-FourRooms-v0'
-    n_threads = 12
+    n_threads = 4
     n_actions = 3
     input_shape = [4, 42, 42]
     env = ParallelEnv(env_id=env_id, num_threads=n_threads, n_actions=n_actions, global_idx=global_ep, input_shape=input_shape, icm=True)
